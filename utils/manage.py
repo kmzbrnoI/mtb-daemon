315d13ded5a477aeabdf4b1576e016309f8ea505
Jan Horacek <me@apophis.cz>
2021-11-15 17:17:44 +0100

    Use proper diag records.

--- utils/manage.py
+++ utils/manage.py
@@ -120,7 +120,7 @@ def module(socket, verbose: bool, module: int) -> None:
 
 
 def module_diag(socket, verbose: bool, module: int) -> None:
-    DVS = ['warnings', 'errors']
+    DVS = ['warnings', 'errors', 'uptime', 'mcu_voltage', 'mcu_temperature']
 
     for dvkey in DVS:
         response = request_response(socket, verbose, {
