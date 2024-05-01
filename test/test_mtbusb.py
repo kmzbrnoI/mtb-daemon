39600a0ab07a6182f9ff4de8f27a7d98f19ed7ca
Jan Horacek <me@apophis.cz>
2024-05-01 12:33:09 +0200

    Testing: add mtbusb speed change test.

--- test/test_mtbusb.py
+++ test/test_mtbusb.py
@@ -2,8 +2,13 @@
 Test 'mtbusb' endpoint of MTB Daemon TCP server using PyTest.
 """
 
-import mtbdaemonif
+from typing import Dict, Any
+import time
+
 import common
+import mtbdaemonif
+
+MTBBUS_SPEEDS = [38400, 57600, 115200, 230400]
 
 mtb_daemon = mtbdaemonif.MtbDaemonIFace()
 
@@ -13,11 +18,7 @@ def test_endpoint_present() -> None:
     mtb_daemon.request_response({'command': 'mtbusb'})  # with different id
 
 
-def test_common_response() -> None:
-    response = mtb_daemon.request_response({'command': 'mtbusb'})
-    assert 'mtbusb' in response
-    mtbusb = response['mtbusb']
-
+def validate_mtbusb_response(mtbusb: Dict[str, Any]) -> None:
     assert 'connected' in mtbusb
     assert isinstance(mtbusb['connected'], bool)
     assert mtbusb['connected'], 'Probably disconnected from MTB-USB'
@@ -28,7 +29,7 @@ def test_common_response() -> None:
 
     assert 'speed' in mtbusb
     assert isinstance(mtbusb['speed'], int)
-    assert mtbusb['speed'] in [38400, 57600, 115200, 230400], 'Invalid MTBbus speed!'
+    assert mtbusb['speed'] in MTBBUS_SPEEDS, 'Invalid MTBbus speed!'
 
     assert 'firmware_version' in mtbusb
     assert isinstance(mtbusb['firmware_version'], str)
@@ -39,6 +40,12 @@ def test_common_response() -> None:
     common.check_version_format(mtbusb['protocol_version'])
 
 
+def test_common_response() -> None:
+    response = mtb_daemon.request_response({'command': 'mtbusb'})
+    assert 'mtbusb' in response
+    validate_mtbusb_response(response['mtbusb'])
+
+
 def test_active_modules() -> None:
     response = mtb_daemon.request_response({'command': 'mtbusb'})
     mtbusb = response['mtbusb']
@@ -46,3 +53,13 @@ def test_active_modules() -> None:
     assert 'active_modules' in mtbusb
     assert isinstance(mtbusb['active_modules'], list)
     assert mtbusb['active_modules'] == [1], 'Only module "1" should be active on the bus!'
+
+
+def test_change_speed() -> None:
+    for speed in MTBBUS_SPEEDS:
+        response = mtb_daemon.request_response({'command': 'mtbusb', 'mtbusb': {'speed': speed}})
+        assert 'mtbusb' in response
+        validate_mtbusb_response(response['mtbusb'])
+        assert response['mtbusb']['speed'] == speed
+        time.sleep(0.5)
+    # End with the highest speed
