056ce1dffd289cb32f15a01b4c015111e6031a11
Jan Horacek <me@apophis.cz>
2024-05-02 10:26:26 +0200

    Testing: add module delete & module create.

--- test/test_module.py
+++ test/test_module.py
@@ -10,7 +10,7 @@ import mtbdaemonif
 mtb_daemon = mtbdaemonif.MtbDaemonIFace()
 
 
-def test_endpointis_present() -> None:
+def test_endpoints_present() -> None:
     mtb_daemon.request_response({'command': 'modules'})
     mtb_daemon.request_response({'command': 'module', 'address': common.TEST_MODULE_ADDR})
     response = mtb_daemon.request_response({'command': 'module'}, timeout=1, ok=False)
@@ -106,5 +106,46 @@ def test_unable_to_delete_active_module() -> None:
     )
     common.check_error(response, common.MtbDaemonError.MODULE_ACTIVE)
 
-# TODO: module_delete
-# TODO: module status change?
+
+def test_delete() -> None:
+    """
+    Warning: this test changes state: it deletes INACTIVE_MODULE_ADDR module.
+    If previous tests in this file would run after this test, they would fail.
+    To restore state, run `test_create` or restart mtb-daemon.
+    """
+    response = mtb_daemon.request_response({'command': 'modules'})
+    assert list(response['modules'].keys()) == \
+        [str(common.TEST_MODULE_ADDR), str(common.INACTIVE_MODULE_ADDR)]
+
+    mtb_daemon.request_response(
+        {'command': 'module_delete', 'address': common.INACTIVE_MODULE_ADDR}
+    )
+
+    response = mtb_daemon.request_response({'command': 'modules'})
+    assert list(response['modules'].keys()) == [str(common.TEST_MODULE_ADDR)]
+
+    response = mtb_daemon.request_response(
+        {'command': 'module', 'address': common.INACTIVE_MODULE_ADDR},
+        timeout=1,
+        ok=False
+    )
+    common.check_error(response, common.MtbDaemonError.MODULE_INVALID_ADDR)
+
+
+def test_create() -> None:
+    """Create deleted module by `test_create` (INACTIVE_MODULE_ADDR)."""
+    response = mtb_daemon.request_response({'command': 'modules'})
+    assert list(response['modules'].keys()) == [str(common.TEST_MODULE_ADDR)]
+
+    module_json = common.MODULES_JSON[common.INACTIVE_MODULE_ADDR]
+    mtb_daemon.request_response({
+        'command': 'module_set_config',
+        'address': common.INACTIVE_MODULE_ADDR,
+        'type_code': module_json['type'],
+        'name': module_json['name'],
+        # omitting 'config' - default config used
+    })
+
+    response = mtb_daemon.request_response({'command': 'modules'})
+    assert list(response['modules'].keys()) == \
+        [str(common.TEST_MODULE_ADDR), str(common.INACTIVE_MODULE_ADDR)]
